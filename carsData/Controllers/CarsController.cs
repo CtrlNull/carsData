ec913868200888cab6bffe47b12567048d6680da
Ralph_Fonz <rfonseca@antioch.edu>
2017-12-18 17:55:54 -0800

    initial commit, created controller, service, html

---
+++ carsData/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using carsData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace carsData.Controllers
+{
+    [RoutePrefix("api/cars")]
+    public class CarsController : ApiController
+    {
+        // {{ Get All }}
+        [HttpGet, Route("getall")]
+        public List<Car> GetAllCars()
+        {
+            CarsService carService = new CarsService();
+            return carService.GetAllCars();
+        }
+        // {{Get by Id }}
+        [HttpGet, Route("getById")]
+        public Car GetById(int id)
+        {
+            return CarsService.GetById(id);
+        }
+        // {{ Create }}
+
+        // {{ Update }}
+
+        // {{ Delete }}
+
+    }
+}
---
+++ carsData/carsService.cs
@@ -0,0 +1,43 @@
+﻿using carsData.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace carsData
+{
+    public class CarsService
+    {
+        public List<Car> GetAllCars()
+        {
+            using (SqlConnection con = new SqlConnection("Server=den1.mssql5.gear.host;Database=carstest;User Id=carstest;Password=Hy19Ks!-Kom3"))
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "cars_getAll";
+                // everytime there is a new row avaliable i will stuff it into that list
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    List<Car> results = new List<Car>();
+                    while (dr.Read())
+                    {
+                        // Create a new Cars object
+                        Car car =  new Car();
+                        car.Id = dr.GetInt32(0);
+                        car.Make = dr.GetString(1);
+                        car.Model = dr.GetString(2);
+                        car.Year = dr.GetByte(3);
+                        car.Color = dr.GetString(4);
+                        // Add that object to the list
+                        results.Add(car);
+                    }
+                    return results;
+                }
+            }
+        }
+    }
+}
+/*
+ * left off at 31.32;
+*/
