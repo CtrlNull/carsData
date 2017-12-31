f46a5b4bdfce571e4dc3934a1fda392aeb78465f
CtrlNull <raphael@fonseca.tech>
2017-12-31 00:02:24 -0800

    getting tired, setting up my database with VSCode

---
+++ carsData/sql.sql
@@ -0,0 +1,25 @@
+-- Adding User Data on the Sql server via query --
+
+-- Create Table auth_user (
+--     Id int Not Null IDENTITY(1,1) Primary Key, 
+--     UserName varchar(15) Not Null, 
+--     UserEmail varchar(20) Not Null, 
+--     UserPassword varchar(255) Not Null
+--     );
+-- Insert Into auth_user (
+--     UserName,
+--     UserEmail,
+--     UserPassword
+-- )
+-- Values (
+--     'Johnny',
+--     'Jaohhnysomething.tech',
+--     '34h23jksdfsdf4hlj42'
+-- );
+--============================================
+
+--========{{ TEST }} ===============
+-- Select * From auth_user;
+
+-- ================================
+
