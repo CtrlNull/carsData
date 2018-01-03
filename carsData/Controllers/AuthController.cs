579fb9ddce3ecf1fe30b487332af9626842217bc
CtrlNull <raphael@fonseca.tech>
2018-01-02 22:27:58 -0800

    added auth controller

---
+++ carsData/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace carsData.Controllers
+{
+    public class AuthController
+    {
+        
+    }
+}
