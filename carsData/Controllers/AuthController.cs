9b167fde488c52393d5719c264b6db8a1c3c1fa4
CtrlNull <raphael@fonseca.tech>
2018-01-02 22:37:04 -0800

    taking a break, saving auth controller

--- carsData/Controllers/AuthController.cs
+++ carsData/Controllers/AuthController.cs
@@ -9,8 +9,33 @@ using System.Web.Security;
 
 namespace carsData.Controllers
 {
-    public class AuthController
+    public class AuthController : ApiController
     {
-        
+        [RoutePrefix("api/auth")]
+        public class LoginRequest
+        {
+            public string UserName {get; set;}
+            public string Password {get; set;}
+        }
+
+        [Route("login"), HttpPost]
+        public HttpReponseMessage Login(LoginRequest req)
+        {
+            var authService = new AuthService();
+            var userId = authService.GetUserId(req.UserName, req.Password);
+
+            if (userId.HasValue)
+            {
+                formsAuthentication.SetAuthCookie(userId.ToString(), createPersistentCooke: true);
+                return Request.CreateResponse(HttpStatusCode.OK,($"user with id {userId} has logged in"));
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid login");
+            }
+        }
+
+
+
     }
 }
